0d8a5b6098dfa3ca5be7dbb977f0726e36900e45
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2025-05-13 18:46:48 +0530

    Create List.dart

---
+++ List.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  
+  List<dynamic> l = [10,20,30];
+  print("Marks $l");
+    
+    
+  List<double> marks = [10,20.3,50,45,30,22,6,13,40,41,35,49];
+  print(marks);
+  
+  
+  List<double> filtered = [];
+  for(int i=0;i<marks.length;i++)
+    if(marks[i]>=40)
+      filtered.add(marks[i]);
+  
+  
+  print("Filtered : "+filtered.toString());
+  
+}
