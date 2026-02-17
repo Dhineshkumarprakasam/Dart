59a5f0a9d85cf405fd612711faf563ee707785c0
Dhinesh kumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2026-02-17 10:00:01 +0530

    Update BottomSheet.dart

--- Flutter/BottomSheet.dart
+++ Flutter/BottomSheet.dart
@@ -1,49 +1,30 @@
 import 'package:flutter/material.dart';
 
-class bottomSheet extends StatelessWidget {
-  const bottomSheet({super.key});
+void showBottomSheet(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    builder: (context) => SizedBox(
+      height: 400,
+      child: Container(child: Center(child: Text("Hello world"))),
+    ),
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Bottom Sheet"),
+        backgroundColor: Colors.blueGrey,
         foregroundColor: Colors.white,
-        backgroundColor: Colors.black,
       ),
-
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            showModalBottomSheet(
-              isScrollControlled: true,
-              context: context,
-              builder: (context) {
-                return Container(
-                  height: 300,
-                  child: Center(
-                    child: GridView(
-                      padding: EdgeInsets.all(20),
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        mainAxisSpacing: 5,
-                        crossAxisSpacing: 5,
-                      ),
-                      children: [
-                        Card(child: Center(child: Text("Button1"))),
-                        Card(child: Center(child: Text("Button2"))),
-                        Card(child: Center(child: Text("Button3"))),
-                        Card(child: Center(child: Text("Button4"))),
-                        Card(child: Center(child: Text("Button5"))),
-                        Card(child: Center(child: Text("Button6"))),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            );
-          },
-          child: Text("Show Bottom Sheet"),
+          onPressed: () => showBottomSheet(context),
+          child: Text("Bottom Sheet"),
         ),
       ),
     );
